f9b8042b8ef43e6d325c0e01761a929d97d11f26
ElhamBadri2411 <elhambadri24@gmail.com>
2024-04-05 01:03:50 -0400

    added blog page

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import MDXRender from "@/components/mdxrender";
+import { getBlogPosts } from "@/lib/blogutils";
+import Image from "next/image";
+type Props = {
+  params: any;
+};
+
+const BlogEntryPage = ({ params }: Props) => {
+  let post = getBlogPosts().find((post) => post.slug === params.slug)!;
+  return (
+    <div className="md:container relative mx-auto w-full overflow-auto p-4 print:p-12 md:p-16">
+      <main className="px-4 py-8 ">
+        <section>
+          <h1 className="pt-10 text-4xl font-semibold max-w-3xl">
+            {post.frontmatter.title}
+          </h1>
+          <div className="mt-2 mb-8 text-sm flex flex-row justify-between max-w-2xl">
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+              {post.frontmatter.date}
+            </p>
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+              {post.frontmatter.length}
+            </p>
+          </div>
+          <div className="flex flex-row items-center justify-center pt-2 pb-4">
+            <Image
+              src={post.frontmatter.image}
+              alt="image"
+              width={400}
+              height={200}
+              className="rounded-lg"
+            />
+          </div>
+          <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+            <MDXRender blogpost={post} />
+          </article>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default BlogEntryPage;
---
+++ src/components/mdxrender.tsx
@@ -0,0 +1,89 @@
+import { BlogPost } from "@/lib/blogutils";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import type { MDXComponents } from "mdx/types";
+import rehypeHighlight from "rehype-highlight";
+import Link from "next/link";
+import hljs, { HighlightOptions } from "highlight.js";
+import Image from "next/image";
+import "@/../public/monokai.css";
+
+type Props = {
+  blogpost: BlogPost;
+};
+
+function RoundedImage(props: any) {
+  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+}
+function CustomLink(props: any) {
+  let href = props.href;
+
+  if (href.startsWith("/")) {
+    return (
+      <Link href={href} {...props} className="text-primary hover:underline">
+        {props.children}
+      </Link>
+    );
+  }
+
+  if (href.startsWith("#")) {
+    return <a {...props} />;
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+}
+
+const componentsCustom: MDXComponents = {
+  h1: ({ children }) => (
+    <h1 className="py-3 text-3xl font-semibold">{children}</h1>
+  ),
+  h2: ({ children }) => (
+    <h2 className=" py-3 text-2xl font-semibold text-primary-secondary">
+      {children}
+    </h2>
+  ),
+  h3: ({ children }) => (
+    <h3 className="py-3 text-xl font-semibold">{children}</h3>
+  ),
+  h4: ({ children }) => <h4 className="">{children}</h4>,
+  h5: ({ children }) => <h5 className="">{children}</h5>,
+  h6: ({ children }) => <h6 className="">{children}</h6>,
+  p: ({ children }) => <p className="py-3 text-base ">{children}</p>,
+  ul: ({ children }) => <ul className="pl-5 list-disc py-3">{children}</ul>,
+  ol: ({ children }) => (
+    <ol className="pl-10 pr-1.5 list-decimal py-3">{children}</ol>
+  ),
+  li: ({ children }) => <li className="py-3">{children}</li>,
+  a: (
+    { href, children } // Add href attribute for links
+  ) => CustomLink({ href, children }),
+  img: (
+    { src, alt } // Add src and alt attributes for images
+  ) => RoundedImage({ src, alt }),
+  code: ({ children }) => (
+    <div className=" w-full pl-1 pr-3 py-5 rounded-md bg-gray-950">
+      <code className="p-1">{children}</code>
+    </div>
+  ),
+
+  // Add more components as needed (address, blockquote, pre, etc.)
+  // ...
+};
+const options = {
+  mdxOptions: {
+    remarkPlugins: [],
+    rehypePlugins: [rehypeHighlight],
+  },
+};
+
+export default async function MDXRender({ blogpost }: Props) {
+  // MDX text - can be from a local file, database, CMS, fetch, anywhere...
+  return (
+    <>
+      <MDXRemote
+        source={blogpost.body}
+        components={componentsCustom}
+        options={options}
+      />
+    </>
+  );
+}
